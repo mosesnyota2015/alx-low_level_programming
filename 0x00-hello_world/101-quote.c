fff114c6cc99c4ad51ee3043a99a03b3755c83e6
Mosesnyota2015 <mosesnyota2015@gmail.com>
2023-06-15 11:43:04 -0700

    Low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,9 @@
 #include <unistd.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main() {
     char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(STDERR_FILENO, message, sizeof(message) - 1);
