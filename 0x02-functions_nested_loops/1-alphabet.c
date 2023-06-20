7af3777b55f3d0ef4c144bcfecacbff97126fe4c
mosesnyota2015 <mosesnyota2015@gmail.com>
2023-06-20 14:43:40 -0700

    Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: not allowed to use starndard libs.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char toprint[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (toprint[i] != '\0')
+	{
+		_putchar(toprint[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
