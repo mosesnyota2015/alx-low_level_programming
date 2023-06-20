0f90bf0142a80da864e1983fcde4c609cfd3d69a
mosesnyota2015 <mosesnyota2015@gmail.com>
2023-06-20 14:20:59 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,22 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - main block
- * Description: prints _putchar, followed by a new line.
+ * main - Print "_putchar" followed by a new line.
+ * Description: not allowed to use starndard libs.
  * Return: 0
  */
+
 int main(void)
 {
-	printf("_putchar\n");
+	char toprint[] = "_putchar";
+	int i = 0;
+
+	while (toprint[i] != '\0')
+	{
+		_putchar(toprint[i]);
+		i++;
+	}
+	_putchar('\n');
+
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+int _putchar(char c);
+#endif
