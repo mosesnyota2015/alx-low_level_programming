192f07c08b23065c233d097eee6147be37153aa1
mosesnyota2015 <mosesnyota2015@gmail.com>
2023-06-21 15:00:25 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,19 +4,6 @@
  *
  * Return: Always 0 (Success)
  */
-void positive_or_negative(int i)
-{
-	int n;
-	n = i;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
-}
-
-
 
 int main(void)
 {
