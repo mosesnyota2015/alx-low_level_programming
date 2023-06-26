e2d838f138bd29012f08e6a44ce4d8a657b82e93
mosesnyota2015 <mosesnyota2015@gmail.com>
2023-06-26 13:17:23 -0700

    Pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - Sets the value of the integer pointed to by n to 98.
+ * @a: Pointer a.
+ * @b: Pointer b.
+ *
+ * Description: This function takes two pointers to an integer and
+ * swaps the values of the two integers
+ */
+void swap_int(int *a, int *b)
+{
+int val_a, val_b;
+val_a = *a;
+val_b = *b;
+
+*a = val_a;
+*b = val_b;
+}
